c2f59ee587d91b1244e3e84480253e229cf9c8ba
Nico <ncfortuin27@gmail.com>
2020-05-28 20:05:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import dog from './dog.jpg';
 import './App.css';
+import { Auth, API, graphqlOperation } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+import * as queries from './graphql/queries';
+import * as mutations from './graphql/mutations';
+import '@aws-amplify/ui/dist/style.css';
+Auth.configure(awsconfig);
+API.configure(awsconfig);
+
+function updateTodo(todo, newDesc){
+  todo['description'] = newDesc;
+  API.graphql(graphqlOperation(mutations.updateTodo, {input: todo}));
+}
+
+function deleteTodo(todo) {
+  API.graphql(graphqlOperation(mutations.deleteTodo, {input: {'id': todo['id']}}));
+}
 
 function App() {
+
+  const userTodos = API.graphql(graphqlOperation(queries.listTodos, {filter: {'name':{'eq':"name" }}}));
+  console.log(userTodos);
+
+  const allTodos = API.graphql(graphqlOperation(queries.listTodos));
+  console.log(allTodos);
+
+  const oneTodo = API.graphql(graphqlOperation(queries.getTodo, { id: "943d98d5-8273-48ec-9f12-5878688cfb47"})).then(function(todo) {
+    updateTodo(todo['data']['getTodo'], "new description");
+    deleteTodo(todo['data']['getTodo']);
+  });
+  console.log(oneTodo);
+
+  Auth.currentAuthenticatedUser({
+    bypassCache: false
+  }).then(function(user) {
+    console.log("User: " + JSON.stringify(user));
+    const todo = { name: user['username'], description: "new todo" };
+    const newTodo = API.graphql(graphqlOperation(mutations.createTodo, { input: todo }));
+}).catch(err => console.log(err));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={dog} className="My Dog" alt="logo" width="300" height="300" />
+        
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
+//export default App;
